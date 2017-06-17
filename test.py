e2e8c36d298f2c16058b1d933c5e72e1c667e687
Cedric Lemaitre <c.lemaitre58@gmail.com>
2017-06-17 12:31:12 +0000

    correct sythax

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import MySQLdb
 
-db = MySQLdb.connect("xxxxxxx.freeboxos.fr", "xxxxx", "xxxx", "elevage")        # name of the data base
+db = MySQLdb.connect("glapeira.freeboxos.fr", "cedric", "cedb002", "elevage")        # name of the data base
 
 # you must create a Cursor object. It will let
 #  you execute all the queries you need
 cur = db.cursor()
 
+
 # Use all the SQL you like
-cur.execute("SELECT * Animal")
+cur.execute("SELECT * from Animal")
 
 # print all the first cell of all the rows
-#for row in cur.fetchall():
-#        print row[0]
+for row in cur.fetchall():
+	print row[0]
 
 db.close()
